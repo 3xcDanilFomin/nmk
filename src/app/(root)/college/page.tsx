448128cd23cde6ac569c7e41f8f5a0806d78933f
danil <ap.fomin203@gmail.com>
2024-12-10 15:43:19 +0300

    fix bag

--- src/app/(root)/college/page.tsx
+++ src/app/(root)/college/page.tsx
@@ -1,10 +1,9 @@
-import { Dropdown } from '@/components';
 
 export default function CollegePage() {
   return (
     <main className='flex flex-col gap-24 py-12'>
       <div className='max-w-screen-xl mx-auto px-3 '>
-        <Dropdown label='Наш колледж' />
+        Колледж
       </div>
     </main>
   );
