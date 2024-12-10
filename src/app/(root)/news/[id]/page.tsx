2479f333de352cf1657d2113ea33649e9b907e97
danil <ap.fomin203@gmail.com>
2024-12-10 15:41:43 +0300

    fix bag

--- src/app/(root)/news/[id]/page.tsx
+++ src/app/(root)/news/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Slider } from '@/components';
 
 export default async function NewsDetailsPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
-
+  console.log(id);
   return (
     <main className='flex flex-col gap-24 py-12'>
       <div className='w-full max-w-screen-xl mx-auto px-3 '>
